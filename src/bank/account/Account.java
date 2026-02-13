8a03d3ac03cf7921e6e3de948263476e23f56c0b
SohamAgawane <sohamagavane@gmail.com>
2026-02-13 20:12:23 +0530

    add equals, hashCode, and toString in Account (identity-based comparison)

--- src/bank/account/Account.java
+++ src/bank/account/Account.java
@@ -2,12 +2,13 @@ package bank.account;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class Account {
-    private int accountNumber;
+    private final int accountNumber;
     private String accountHolderName;
     private double balance;
-    private long timestamp;
+    private final long timestamp;
 
     public Account(int accountNumber, String accountHolderName, double balance) {
         this.accountNumber = accountNumber;
@@ -28,17 +29,19 @@ public class Account {
         return balance;
     }
 
-    protected void deductAmount(double amount) {
-        balance -= amount;
-    }
-
     public long getTimestamp() {
         return timestamp;
+    }
+
+    protected void deductAmount(double amount) {
+        balance -= amount;
     }
 
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount");
         }
     }
 
@@ -60,9 +63,25 @@ public class Account {
         SimpleDateFormat sdf =
                 new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
-        return "Account Number: " + accountNumber +
-                "\nAccount Holder Name: " + accountHolderName +
-                "\nBalance: " + balance +
-                "\nCreated At: " + sdf.format(date);
+        return "Account{" +
+                "accountNumber=" + accountNumber +
+                ", accountHolderName='" + accountHolderName + '\'' +
+                ", balance=" + balance +
+                ", createdAt=" + sdf.format(date) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof Account)) return false;
+        Account account = (Account) o;
+        return accountNumber == account.accountNumber;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber);
     }
 }
